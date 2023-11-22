2a925165b6dc235d3c4a12445467e4b9fb15b291
unknown <yatharthanand2410@gmail.com>
2023-11-22 20:10:39 +0530

    changed the architecture of deep auto encoder

--- models/AutoEncoder/deep_ae.py
+++ models/AutoEncoder/deep_ae.py
@@ -20,24 +20,32 @@ class Encoder(nn.Module):
     def __init__(self,device, indim, outdim=512):
         super(Encoder, self).__init__()
         self.linear1 = nn.Linear(indim,8192,device=device)
-        self.linear2 = nn.Linear(8192,2048,device=device)
-        self.linear3 = nn.Linear(2048,outdim,device=device)
+        self.linear2 = nn.Linear(8192,4096,device=device)
+        self.linear3 = nn.Linear(4096,2048,device=device)
+        self.linear4 = nn.Linear(2048,1024,device=device)
+        self.linear5 = nn.Linear(1024,outdim,device=device)
 
     def forward(self, x):
         x = nn.SELU()(self.linear1(x))
         x = nn.SELU()(self.linear2(x))
-        x = nn.Sigmoid()(self.linear3(x))
+        x = nn.SELU()(self.linear3(x))
+        x = nn.SELU()(self.linear4(x))
+        x = nn.Sigmoid()(self.linear5(x))
         return x
 
 
 class Decoder(nn.Module):
     def __init__(self,device,outdim, indim=512):
         super(Decoder, self).__init__()
-        self.linear3 = nn.Linear(indim,2048,device=device)
-        self.linear2 = nn.Linear(2048,8192,device=device)
+        self.linear5 = nn.Linear(indim,1024,device=device)
+        self.linear4 = nn.Linear(1024,2048,device=device)
+        self.linear3 = nn.Linear(2048,4096,device=device)
+        self.linear2 = nn.Linear(4096,8192,device=device)
         self.linear1 = nn.Linear(8192,outdim,device=device)
 
     def forward(self, x):
+        x = nn.SELU()(self.linear5(x))
+        x = nn.SELU()(self.linear4(x))
         x = nn.SELU()(self.linear3(x))
         x = nn.SELU()(self.linear2(x))
         x = nn.Sigmoid()(self.linear1(x))
